afae57151e985a83c4817ca29be5c6d8a9777233
student apnaCollage <shubhamsahu410@gamil.com>
2024-08-24 22:52:47 +0530

    CICD Workflow

---
+++ python.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello, World!")
+    
+if __name__ == "__main__":
+    main()
+
+# write code to run the python script
+def run_python_script():
+        subprocess.run(["python", "python.py"], cwd="PythonWorkflow")
+
+        
+
